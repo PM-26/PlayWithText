f5f508e54f4eaf0e732d88b0580130cbd695812f
PM <107318440+PM-26@users.noreply.github.com>
2023-09-23 06:09:05 +0000

    nextline problem fixed

--- pm/src/components/Form.js
+++ pm/src/components/Form.js
@@ -40,10 +40,10 @@ export default function Form(props) {
         </div>
         <div className='container my-3'>
             <h2>Your Text Summary</h2>
-            <p>{text.split(" ").length} Words, {text.length} Characters</p>
-            <p>{0.008*text.split(" ").length} Minutes Read</p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Words, {text.length} Characters</p>
+            <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes Read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Nothing to Preview!"}</p>
 
         </div>
         </>
